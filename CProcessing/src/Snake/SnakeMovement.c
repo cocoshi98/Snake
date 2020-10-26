8aaf3c96a163980e39f3e2fee446f0369ec72f06
Jamie Kong <j.kong@digipen.edu>
2020-10-26 17:09:20 +0800

    Added Snake movement functionality, SnakeMovement.c

---
+++ CProcessing/src/Snake/SnakeMovement.c
@@ -0,0 +1,25 @@
+#ifndef _SNAKE_MOVEMENT_
+#define _SNAKE_MOVEMENT_
+
+#include "snek.h"
+void MoveSnake(Snake* m_snake, bool* canMove, float* timer)
+{
+	if (m_snake->canMove) {
+		//increment snake
+		if (!m_snake->isDead) {
+			if (m_snake->segmentLength > 1) {
+				for (int x = m_snake->segmentLength; x > 0; x--)
+				{
+					m_snake->segments[x].pos.x = m_snake->segments[x - 1].pos.x;
+					m_snake->segments[x].pos.y = m_snake->segments[x - 1].pos.y;
+				}
+			}
+			m_snake->segments[0].pos.x += (int)m_snake->movVec.x;
+			m_snake->segments[0].pos.y += (int)m_snake->movVec.y;
+		}
+		m_snake->canMove = false;
+	}
+}
+
+#endif // !
+
